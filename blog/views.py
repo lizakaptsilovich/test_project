eb16bfb98e1f05cbcc3fdccf4edd12f4026a5392
Liz <liza.kaptsilovich@gmail.com>
2025-09-28 21:40:00 +0200

    adding html

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,7 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
-
-def home_page(request):
-    return HttpResponse("you made it!")
+    posts=Post.objects.order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
